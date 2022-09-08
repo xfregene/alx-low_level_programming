d14d6a1f30eebb41f49889e85b8f899f0e25d1d9
Esere Fregene <xfregene@gmail.com>
2022-09-08 15:51:46 -0700

    Print string to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - The program's entry point
+*
+* Return: 1 (if successful)
+*/
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr );
+
+	return (1);
+}
