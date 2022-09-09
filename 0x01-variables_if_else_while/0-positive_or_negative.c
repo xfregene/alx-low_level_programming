384f61aebbadcbabbacfa9083f1240a84f4b7923
Esere Fregene <xfregene@gmail.com>
2022-09-09 15:07:02 -0700

    Correction of print message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	/*Code to check the value of the number*/
 	if (n > 0)
 	{
-		printf("%d is greater than 0\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is 0\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is less than 0\n", n);
+		printf("%d is negative\n", n);
 	}
 
 	return (0);
