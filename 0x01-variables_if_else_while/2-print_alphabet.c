b7c3671c3c6322a47d3cf7990ca3b692f6b1b3a4
Esere Fregene <xfregene@gmail.com>
2022-09-09 16:30:23 -0700

    Print the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0 (if successful)
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+}
