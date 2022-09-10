5104b3e1736dd0e9e8d61d02d609dc22b232b2a8
Esere Fregene <xfregene@gmail.com>
2022-09-09 17:30:12 -0700

    Print alphabet, except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0 (if successful)
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
