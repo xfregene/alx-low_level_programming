84bb68290a4eef9e80d83d77b65e1a910584f19d
Esere Fregene <xfregene@gmail.com>
2022-09-09 17:09:17 -0700

    Print alphabet in both lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,18 @@
 *
 * Return: 0 (if successful)
 */
-int main()
+int main(void)
 {
 	char alpha;
-	        
-	for (alpha = 'a'; alpha <= 'z'; alpha ++)
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		putchar(alpha);
 	}
-	for (alpha = 'A'; alpha <= 'Z'; alpha ++)
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 	{
 		putchar(alpha);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
