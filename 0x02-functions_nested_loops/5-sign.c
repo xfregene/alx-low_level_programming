4986d49746435ff9c9d3c5994365f2a81026c4f4
Esere Fregene <xfregene@gmail.com>
2022-09-14 08:28:59 -0700

    Added description for the print_sign function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
-/***
+/**
 * print_sign - Prints the sign of a number
 *
 * @n: The integer number that is taken as input.
+*
+* Description: Checks the input and prints whether the
+* number is positive, negative, or zero.
 *
 * Return: 1 if n is positive, 0 if it's 0, -1 if it's negative.
 */
