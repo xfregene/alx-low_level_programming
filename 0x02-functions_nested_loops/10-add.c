b0a6b7fff453f16d8fa3976f753b9f0e59d7a978
Esere Fregene <xfregene@gmail.com>
2022-09-14 21:54:19 -0700

    Adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - adds two integers
+*
+* @a: first number
+* @b: second number
+*
+* Return: Summation of both numbers.
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
