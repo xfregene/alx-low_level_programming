9913e636667ccbb37db1d9ecc6d219495d09ba07
Esere Fregene <xfregene@gmail.com>
2022-09-20 21:57:09 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_rev - Prints a string in reverse
+* @s: string input value
+*
+* Return: void
+*/
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (len -= 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
