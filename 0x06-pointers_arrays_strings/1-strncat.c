00618813176508db16e870ad9699dbf437a43df7
Esere Fregene <xfregene@gmail.com>
2022-09-23 21:07:51 -0700

    Concatenate two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,15 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-
-	for (j = 0; src[j] != '\0' && j < n j++)
+	while (dest[i] != 0)
 	{
-		dest[i + j] = src[j];
+		i++;
 	}
-	dest[i + j] = '\0';
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
