6d07d847b22c6855412cd7d15a548abb5d092bf9
Esere Fregene <xfregene@gmail.com>
2022-09-26 22:27:10 -0700

    Debugging

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,11 @@
 */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (*s == c)
+			return (s);
+		s++;
 	}
-	if (*s == c)
-		return (s);
 	return (0);
 }
