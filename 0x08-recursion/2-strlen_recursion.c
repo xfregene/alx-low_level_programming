4b9fc336a8824596e45bbe705bd426c7ea823f0f
Esere Fregene <xfregene@gmail.com>
2022-09-28 16:32:11 -0700

    Display the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - Returns the length of a string
+*
+* @s: String
+*
+* Return: String length
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s + 1) +  1);
+}
