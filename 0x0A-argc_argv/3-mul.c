aac508eeb1ee6131de5216854823986a7c6d6a2d
Esere Fregene <xfregene@gmail.com>
2022-09-30 21:03:28 -0700

    Multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Multiplies two numbers
+* @argc: Argument counter
+* @argv: Argument vector
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	num1 = argv[1];
+	num2 = argv[2];
+	printf("%d\n", num1 * num2);
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
