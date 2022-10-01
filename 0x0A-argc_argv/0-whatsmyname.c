fd4bde929653832d5d20fb808b2f3ab1fceff78a
Esere Fregene <xfregene@gmail.com>
2022-09-30 19:26:37 -0700

    Create project folder, README file and initial program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* @argc: Argument count
+* @argv: Argument vector
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("This program is called: \"%s\"\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0x0A-argc_argv is a project about argument count and argument vector.
