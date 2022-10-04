3c581465c8c833b8aa0cff14e66248c0569e0c37
Esere Fregene <xfregene@gmail.com>
2022-10-04 04:51:15 -0700

    Added while loop to duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,18 +10,21 @@
 char *_strdup(char *str)
 {
 	char *newstr;
-	int i;
+	int i, j;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	newstr = malloc(sizeof(*newstr));
-	i = 0;
-	while (str[i])
-	{
-		newstr[i] = str[i];
+	i = 1;
+	while (str[i] != '\0')
 		i++;
+	newstr = malloc(sizeof(*str) * i);
+	j = 0;
+	while (str[j])
+	{
+		newstr[j] = str[j];
+		j++;
 	}
 	if (newstr == NULL)
 	{
