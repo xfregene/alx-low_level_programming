203b6c7b9466e8c94222848bd970978fb6f1ceeb
Esere Fregene <xfregene@gmail.com>
2022-10-04 17:55:48 -0700

    Updated the code and added whitespaces

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,13 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+* _strlen - Calculates the length of a string
+* @s: String
+*
+* Return: String length
+*/
 int _strlen(char *s)
 {
-if (*s == '\0')
-{
-return (0);
-}
-return (1 + _strlen(s + 1));
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen(s + 1));
 }
 
 /**
@@ -19,27 +25,31 @@ return (1 + _strlen(s + 1));
 */
 char *str_concat(char *s1, char *s2)
 {
-char *s3;
-int i, j, s1len, s2len;
+	char *s3;
+	int i, j, s1len, s2len;
 
-s1len = _strlen(s1);
-s2len = _strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	s1len = _strlen(s1);
+	s2len = _strlen(s2);
 
-s3 = malloc(sizeof(s3) * (s1len + s2len));
-if (s3 == NULL)
-{
-return (NULL);
-}
-i = 0;
-while (s1[i] != '\0')
-{
-s3[i] = s1[i];
-i++;
-}
-for (j = 0; s2[j] != '\0'; j++, i++)
-{
-s3[i] = s2[j];
-}
-s3[i] = '\0';
-return (s3);
+	s3 = malloc(sizeof(char) * (s1len + s2len));
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	for (j = 0; s2[j] != '\0'; j++, i++)
+	{
+		s3[i] = s2[j];
+	}
+	s3[i] = '\0';
+	return (s3);
 }
